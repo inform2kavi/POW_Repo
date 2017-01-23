6cc26d7f6819b804884c66376adfe69b70b06677
Kavitha <inform2kavi@gmail.com>
2017-01-23 13:13:37 +1100

    Creating POW admin Item Management Class file

---
+++ src/main/java/utils/POW_Admin_ItemManagement.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class POW_Admin_ItemManagement {
+	
+	WebDriver driver;
+	private By Home = By.xpath("");
+	
+	
+	public POW_Admin_ItemManagement(WebDriver driver)
+	{
+		this.driver   = driver;
+	}
+	
+	
+
+}
