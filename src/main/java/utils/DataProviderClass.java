6326f81bd29b7aa84d2cc550ea93660f0e08b021
Kavitha <inform2kavi@gmail.com>
2017-03-10 13:47:31 +1100

    Updating Data Providr Class

--- src/main/java/utils/DataProviderClass.java
+++ src/main/java/utils/DataProviderClass.java
@@ -34,47 +34,6 @@ public class DataProviderClass {
 			
 		}
 		
-		
-		if(m.getName().equals("firstmethod"))
-		{
-			int sheetnum =2;
-		
-			
-			LoginObj = new Object[1][3];
-		
-			for(int i=0; i<1; i++)
-			{		
-				for(int j=0;j<3; j++)		
-				{
-					LoginObj[i][j] = ds.getvalue(sheetnum, i+1, j);
-					System.out.println("Details "+LoginObj[i][j]);				
-			
-		    
-				}
-			}
-		
-		}
-		
-		if(m.getName().equals("secondmethod"))
-		{
-			int sheetnum =3;
-			
-			//DataReader ds = new DataReader("/Users/sankavi/Workbook2.xlsx");
-			LoginObj = new Object[1][2];
-			
-			for(int i=0; i<1; i++)
-			{		
-				for(int j=0;j<2; j++)		
-				{
-					LoginObj[i][j] = ds.getvalue(sheetnum, i+1, j);
-					System.out.println("Details "+LoginObj[i][j]);				
-				
-			    
-				}
-			}
-			
-		}
-		
 		if(m.getName().equals("Login"))
 		{
 			System.out.println("Reading Dataprovider");
